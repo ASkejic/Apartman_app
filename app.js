b140855accc74bfaf813ca3d8d711b1aa021e4b7
Ante Skejić <ante.skejic@gmail.com>
2017-09-28 20:42:06 +0200

    Basic app setup with routes and template

---
+++ app.js
@@ -0,0 +1,23 @@
+
+//// Var init
+
+var express = require('express');
+var app = express();
+
+
+//// Routes init
+var indexRoutes = require("./routes/index.js");
+
+
+//// App setup
+app.set("view engine", "ejs");
+
+//// Routes config
+app.use(indexRoutes);
+
+
+
+//// Port listening
+app.listen(3000, function () {
+  console.log('App running on port 3000!');
+});
